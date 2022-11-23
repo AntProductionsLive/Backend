f9ec443fd0fca93ec9ff2759d668b4b47e4d4ce2
AntProductionsLive <83663575+AntProductionsLive@users.noreply.github.com>
2022-11-23 12:13:22 +0100

    Delete ant-productions-crud directory

--- ant-productions-crud/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '78f2d44159525e9cf4aa120337a57738'),
-    },
-  },
-});
--- ant-productions-crud/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '4e3148d0-0737-4bfb-9754-7e622d7ffb3f'
-};
